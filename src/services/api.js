7bdeb2af9ddf90fd34de058ad58872119569e9fa
Danilo <danilopradella@hotmail.com>
2026-07-23 21:40:32 -0300

    adicionado services/api.js com interceptors de autenticacao jwt + removido estilos e assets/

---
+++ src/services/api.js
@@ -0,0 +1,48 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:8080",
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      try {
+        const refreshToken = localStorage.getItem("refreshToken");
+
+        const { data } = await axios.post("http://localhost:8080/auth/refresh", {
+          refreshToken: refreshToken,
+        });
+
+        localStorage.setItem("token", data.token);
+        localStorage.setItem("refreshToken", data.refreshToken);
+
+        originalRequest.headers.Authorization = `Bearer ${data.token}`;
+
+        return api(originalRequest);
+      } catch (refreshError) {
+        localStorage.removeItem("token");
+        localStorage.removeItem("refreshToken");
+        window.location.href = "/login";
+        return Promise.reject(refreshError);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+export default api;
